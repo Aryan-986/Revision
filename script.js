b90ea9192f025a0cf0a2af577d4e077d7708c3d5
Aryan-986 <aryankarki984@gmail.com>
2025-02-01 22:04:01 +0545

    Basic javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("code is running")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to blast your computer")
+
+if(isTrue) {
+    console.log("Compuer is blasting")
+}
+
+else{
+    console.log("computer is not blasting chill")
+}
+
+console.log("Your number is "+a)
+
+document.title= "Hey i am good"
+
+document.body.style.backgroundColor = "red"
