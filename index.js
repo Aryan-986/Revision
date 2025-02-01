de5d8ab137373512dea464990a1a0f04f91963a3
Aryan-986 <aryankarki984@gmail.com>
2025-02-02 00:34:15 +0545

    git javascript function loop

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-console.log("My name ai aryan karki")
+function nice(name) {
+    console.log("Hey "+ name +" you are nice")
+    console.log("Hey "+ name +" you are good")
+    console.log("Hey "+ name +" you are bad")
+    console.log("Hey "+ name +" you are tall")
+    console.log("Hey "+ name +" you are weak")
+}
 
-// let a=1;
-// // console.log(a)
-// // console.log(a+1)
-// // console.log(a+2)
+// nice("Karki")
 
-// for (let index = 0; index < 100; index++) {
-//     console.log(a+index);
-    
-// }
+function sum(a, b, c = 3) {
+    //console.log(a + b)
+    return a + b + c
+}
 
-// let object = {
-//     name: "Aryan",
-//     role:"Programmer",
-//     company: "CodewithAryan Ai"
-// }
+result1 = sum (3, 5)
+result2 = sum (13, 5)
+result3 = sum (3, 15)
 
-// for (const key in object) {
-//         const element = object[key];
-//         console.log(key,element)
-        
-//     }
+// nice("Karki")
+// nice("jungey")
 
-// let i = 0;
-// while (i < 6) {
-// console.log(i)
-// i++;
-// }
+console.log("The sum of these number is: ", result1)
+console.log("The sum of these number is: ", result2)
+console.log("The sum of these number is: ", result3)
 
-let i = 0
-do {
-    console.log(i)
-    i++;
-}while(i<6);
+const func1 = (x)=> {
+    console.log("I am an arrow function", x)
+}
+
+func1(34);
+func1(66);
+func1(84);
