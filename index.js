a36c4a49d9d50dd3378397793fdfd9831f98b618
Aryan-986 <aryankarki984@gmail.com>
2025-02-01 23:42:41 +0545

    else if, else ladder

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello i am aryan karki")
+
+let age = 0;
+
+
+//if((age+grace)>18) {
+//    console.log("You can drive");
+//}
+
+//else {
+  //  console.log("You cannot drive");
+//}
+
+if(age==18){
+    console.log('You can drive');
+}
+
+else if(age == 0) {
+    console.log("Are you kidding?")
+}
+
+else{
+    console.log("You cannot drive");
+}
