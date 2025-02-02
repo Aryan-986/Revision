4ef8c34cab01f48e61dbce4856e33bf931382e31
Aryan-986 <aryankarki984@gmail.com>
2025-02-02 16:32:39 +0545

    calculating factorial  number with reduce and for loop mehtod

--- mfr.js
+++ mfr.js
@@ -1,34 +1,39 @@
-let array = [1, 13, 5, 7, 11];
-// let newarray = [];
+// let number = parseInt(prompt("Enter your number")); // Convert string input to number
 
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-//     newarray.push(element**2);
-// }
+// if (number === 0 || number === 1) {
+//     console.log("The factorial of the given number is: 1");
+// } else {
+//     let array = [];
+//     for (let index = 1; index <= number; index++) {
+//         array.push(index);
+//     }
 
-// console.log(newarray);
+//     console.log("Generated array: ", array);
 
-//short method this is why we use map
-let newarray = array.map((e)=>{
-    return e **2
-})
+//     const red = (a, b) => a * b;
 
-console.log(newarray)
+//     console.log("The factorial of the given number is: ", array.reduce(red));
 
 
-//filter functions
-const greaterThanSeven = (e)=> {
-    if(e>7){
-        return true
+//conver number from string to integer using parseint().
+//Loop runs correctly from 1 to number (inclusive) to generate an array of numbers.
+//fixing array.push index to correctly populated array.
+//Handled 0 and 1 edge cases to return 1 directly without unnecessary calculations.
+
+
+
+
+// for loop method
+
+let number = parseInt(prompt("Enter your number")); // Converting input into an integer
+let factorial = 1;
+
+if (number === 0 || number === 1) {
+    factorial = 1;
+} else {
+    for (let index = 2; index <= number; index++) {
+        factorial *= index; // Multiply factorial by each number up to 'number'
     }
-    return false
-}
-console.log(newarray.filter(greaterThanSeven))
-
-let array2 = [1,2,3,4,5,6]
-
-const yellow = (a,b)=>{
-    return a+b
 }
 
-console.log(array2.reduce(yellow))
+console.log(`The factorial of ${number} is: ${factorial}`);
